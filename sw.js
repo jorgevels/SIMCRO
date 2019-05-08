d098b1cd744226ee2ec3d49d524c4f05f8f4a432
jorgevel <jorgevelasquez006@hotmail.com>
2019-05-07 19:31:35 -0500

    Corrigiendo el title de la aplicacion

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
-self.addEventListener('install', function(e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open('video-store').then(function(cache) {
+    caches.open("video-store").then(function(cache) {
       return cache.addAll([
-       
-        '/SIMCRO/',
-        '/SIMCRO/index.html',
-        '/SIMCRO/indexx.js',
-        '/SIMCRO/css/index.css',
-        '/SIMCRO/style.css',
-        
+        "/SIMCRO/",
+        "/SIMCRO/index.html",
+        "/SIMCRO/indexx.js",
+        "/SIMCRO/css/index.css",
+        "/SIMCRO/style.css"
       ]);
     })
   );
- });
- 
- self.addEventListener('fetch', function(e) {
-   console.log(e.request.url);
-   e.respondWith(
-     caches.match(e.request).then(function(response) {
-       return response || fetch(e.request);
-     })
-   );
- });
- 
+});
+
+self.addEventListener("fetch", function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
